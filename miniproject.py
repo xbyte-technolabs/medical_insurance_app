dd3901bd754ebd2e7585cfee3f44d6035bd30e15
vikas <vikas@gmail.com>
2024-08-05 17:05:31 +0530

    changes

--- miniproject.py
+++ miniproject.py
@@ -125,4 +125,3 @@ if menu=='Insurance Cost':
         prediction = model.predict(input_data)
         output = f'Predicted Insurance Cost : {prediction[0]:.2f}  💸'
         st.markdown(f"<h6 style='font-size:20px;'>{output.strip()}</h1>", unsafe_allow_html=True)
- 
